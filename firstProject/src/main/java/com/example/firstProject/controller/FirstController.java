1bf93f4ab8462007d7bb07f1ce19bdb3bcaf5997
Shuung <qmoy2510@gmail.com>
2025-06-19 15:16:34 +0900

    'addStyle'

--- firstProject/src/main/java/com/example/firstProject/controller/FirstController.java
+++ firstProject/src/main/java/com/example/firstProject/controller/FirstController.java
@@ -7,14 +7,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class FirstController {
     String name = "박현빈";
-   @GetMapping("/hi")
+   @GetMapping("/")
    public String hello(Model model) {
            model.addAttribute("name", name);
         return "greetings";
-    }
+    }   
     @GetMapping("/bye")
     public String bye(Model model) {
            model.addAttribute("name", name);
-        return "byebye";
+        return "by ebye";
     }
 }
