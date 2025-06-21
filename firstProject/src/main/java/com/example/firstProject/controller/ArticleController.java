88761683d62751216e5f981a6273686a9aaa2098
qmoy2510 <qmoy2510@gmail.com>
2025-06-22 00:04:50 +0900

    롬복으로 리펙토링

--- firstProject/src/main/java/com/example/firstProject/controller/ArticleController.java
+++ firstProject/src/main/java/com/example/firstProject/controller/ArticleController.java
@@ -3,11 +3,13 @@ package com.example.firstProject.controller;
 import com.example.firstProject.dto.ArticleForm;
 import com.example.firstProject.entity.Article;
 import com.example.firstProject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j
 @Controller
 public class ArticleController {
 
@@ -21,14 +23,14 @@ public class ArticleController {
 
     @PostMapping("/article/create")
     public String createArticle (ArticleForm form) {
-        System.out.println(form.toString());
+        log.info(form.toString());
         //1. DTO 를 엔티티로 변환
         Article article = form.toEntity();
-        System.out.println(article.toString());
+        log.info(article.toString());
 
         //2. 레포지터리로 엔티티를 db에 저장
         Article saved = articleRepository.save(article);
-        System.out.println(saved.toString());
+        log.info(saved.toString());
         return "";
     }
 }
