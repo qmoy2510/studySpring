52e410443173b2114c1080cb2795988c346c4529
Shuung <qmoy2510@gmail.com>
2025-06-26 17:45:07 +0900

    단일 데이터 조회

--- firstProject/src/main/java/com/example/firstProject/controller/ArticleController.java
+++ firstProject/src/main/java/com/example/firstProject/controller/ArticleController.java
@@ -4,9 +4,12 @@ import com.example.firstProject.dto.ArticleForm;
 import com.example.firstProject.entity.Article;
 import com.example.firstProject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.aspectj.asm.IModelFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j
@@ -19,6 +22,17 @@ public class ArticleController {
     @GetMapping("/article/new")
     public String newArticleFomr () {
         return "articles/new";
+    }
+
+    @GetMapping("/article/{id}")
+    public String show(@PathVariable Long id , Model model) {
+        log.info("id = " +id);
+        //1. id를 조회해 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        //2. 모델에 데이터 등록하기
+        model.addAttribute("article", articleEntity);
+        //3. 뷰 페이지 반환하기
+        return "articles/show";
     }
 
     @PostMapping("/article/create")
