4924243d08e2882f20e0024799dac844753dee02
Shuung <qmoy2510@gmail.com>
2025-07-09 00:06:50 +0900

    article 더미 데이터 추가

---
+++ firstProject/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into article(id, title, content) values(1, '김치찌개', '맛있습니다');
+insert into article(id, title, content) values(2, '계란말이', '고소합니다');
+insert into article(id, title, content) values(3, '제육볶음', '매콤합니다');
